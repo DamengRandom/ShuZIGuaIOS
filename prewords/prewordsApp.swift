b74a67facd1906f875e308b5b9f9d4ded958e4f6
damengrandom <damonmaozewu@gmail.com>
2023-06-16 21:33:59 +1000

    Initial Commit

---
+++ prewords/prewordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  prewordsApp.swift
+//  prewords
+//
+//  Created by Damon Wu on 16/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct prewordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
