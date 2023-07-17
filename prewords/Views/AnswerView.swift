021387a4186cae33a23f8b0f3c12ea9799a4e0c8
damengrandom <damonmaozewu@gmail.com>
2023-07-17 20:28:47 +1000

    added youtube video reader function

--- prewords/Views/AnswerView.swift
+++ prewords/Views/AnswerView.swift
@@ -6,17 +6,38 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct AnswerView: View {
     @Binding var output: String
 
     var body: some View {
-        Text("卦爻: \(output)")
+        let theAnswer = getAnwers(guaYaoName: output)
+        Text("卦爻: \(theAnswer.guaName)\(theAnswer.yaoName)")
+        
+        Video(videoURL: theAnswer.videos[0]).frame(width: 350, height: 190).cornerRadius(12).padding(.horizontal, 24)
     }
 }
 
 struct AnswerView_Previews: PreviewProvider {
     static var previews: some View {
         AnswerView(output: .constant("test value"))
+    }
+}
+
+struct Video: UIViewRepresentable {
+    let videoURL: String
+
+    func makeUIView(context: Context) -> some WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        guard let YoutubeURL = URL(string: videoURL) else {
+            return
+        }
+        
+        uiView.scrollView.isScrollEnabled = false
+        uiView.load(URLRequest(url: YoutubeURL))
     }
 }
